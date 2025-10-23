fea38630d1922f7cd55b305dba580a0ffe54d9d4
Policki <eenej31@gmail.com>
2025-10-23 09:36:58 +0200

    Add files via upload

---
+++ vaja_6_2/vaja_6_2/mai_enej_tine_KVADER-main/vaja_6_2/vaja_6_2/Kvader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace vaja_6_2
+{
+    public class Kvader
+    {
+        private double a;
+        private double b;
+
+        public Kvader()
+        {
+            a = 1;
+            b = 1;
+        }
+
+        public Kvader(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void Nastavi()
+        {
+            Console.Write("Vnesi prvo stranico: ");
+            double a= double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("Vnesi drugo stranico: ");
+            double b = double.Parse(Console.ReadLine());
+            this.a = a;
+            this.b = b;
+        }
+        public void VrniObseg()
+        {
+            double o = (2 * a) + (2 * b);
+            Console.WriteLine("Obseg Kvadra je: {0}", o);
+        }
+
+        public void VrniPloscino()
+        {
+            double p = a * b;
+            Console.WriteLine("Ploščina Kvadra je: {0}", p);
+        }
+    }
+}
---
+++ vaja_6_2/vaja_6_2/mai_enej_tine_KVADER-main/vaja_6_2/vaja_6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaja_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prvi naj ima širino in višino enako 3,
+            //drugi pa naj ima širino 6 in višino 4.
+            //Z uporabo metod na standardni izhod izpiši njuni ploščini.
+            Kvader k1 = new Kvader(3, 3);
+            Kvader k2 = new Kvader(6, 4);
+
+            Console.WriteLine("Kvader 1");
+            k1.VrniObseg();
+            k1.VrniPloscino();
+            Console.WriteLine("");
+            Console.WriteLine("Kvader 2");
+            k2.VrniObseg();
+            k2.VrniPloscino();
+
+            Console.ReadKey(true);
+        }
+    }
+}
